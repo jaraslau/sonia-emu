d5158bc65a0a5aa503161abd01df4eb97204ece3
JarasIau <jaraslau@tutanota.com>
2025-07-06 17:59:35 +0200

    Fixed top-buttons' position

--- webui/static/js/options.js
+++ webui/static/js/options.js
@@ -3,12 +3,15 @@ document.addEventListener('DOMContentLoaded', () => {
     toggle.addEventListener('change', () => {
         const analog = document.querySelectorAll('.analog-trigger');
         const digital = document.querySelectorAll('.button-trigger');
+	const topButtons = document.getElementById('top-buttons');
         if (toggle.checked) {
             analog.forEach(el => el.style.display = 'flex');
             digital.forEach(el => el.style.display = 'none');
+	    topButtons.style.top = '-10%';
         } else {
             analog.forEach(el => el.style.display = 'none');
             digital.forEach(el => el.style.display = 'flex');
+	    topButtons.style.top = '10%';
         }
     });
 });
